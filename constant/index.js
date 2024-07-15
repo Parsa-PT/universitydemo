8c2dd2ade7777c7093d98cd1219086689ef505dd
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-07-15 13:41:30 +0330

    Update index.js

--- constant/index.js
+++ constant/index.js
@@ -3,25 +3,25 @@
 export const SEC = [
     {
         id:1,
-        pic:'/pic2.png',
+        pic:'./pic2.png',
         title:'درس کارورزی',
         name:'استاد محمود ایزدپرست'
     },
     {
         id:2,
-        pic:'/pic1.jpg',
+        pic:'./pic1.jpg',
         title:'درس کارورزی',
         name:'استاد ویدا مراقبتی'
     },
     {
         id:1,
-        pic:'/pic3.jpg',
+        pic:'./pic3.jpg',
         title:'درس کاربینی و کارورزی',
         name:'استاد سید جوادی'
     },
     {
         id:1,
-        pic:'/pic4.jpg',
+        pic:'./pic4.jpg',
         title:'پروژه فناوری اطلاعات',
         name:'استاد نسرین نصر آبادی'
     },
