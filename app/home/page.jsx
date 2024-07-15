b519c3b60bb0eb052649be8018b9a5d27548aa75
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-07-15 22:02:27 +0330

    Update page.jsx

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import Navbar from '@/components/Navbar'
-import HOME from '@/components/Home'
-import Sec1 from '@/components/Sec1'
-import Sec2 from '@/components/Sec2'
+import Register from '../Register'
 
 const page = () => {
   return (
     <div>
-      <Navbar/>
-      <HOME/>
-      <Sec1/>
-      <Sec2/>
+       <Register/>
     </div>
   )
 }
