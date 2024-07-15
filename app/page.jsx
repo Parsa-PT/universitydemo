ff696d01b0f81f234b7f7546141e3618158b3d43
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-07-15 13:38:33 +0330

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,19 +3,24 @@ import HOME from "@/components/Home";
 import Image from "next/image";
 import Sec1 from "@/components/Sec1";
 import Sec2 from "@/components/Sec2";
-import Register from "./Register";
-import Cookies from "js-cookie";
+
+
 
 export default function Home() {
-
+  
 
   return (
     <div>
 
       
- 
-        <Register/>
+
       
+      <Navbar/>
+      <HOME/>
+      <Sec1/>
+      <Sec2/>
+
+
       
       
       
